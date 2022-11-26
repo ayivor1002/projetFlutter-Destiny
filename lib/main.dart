7aa991f9463687f6c90bb35e64edb965ede43400
ayivor1002 <mawuli.ayivor@ipnetinstitute.com>
2022-11-26 20:57:58 +0000

    projet destiny

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:destiny/cerveauHistoire.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Destiny());
+}
+
+
+CerveauHistoire cerveau= CerveauHistoire();
+
+class Destiny extends StatelessWidget {
+  const Destiny({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home:PageHistoire() ,
+    );
+  }
+}
+
+class PageHistoire extends StatefulWidget {
+  const PageHistoire({Key? key}) : super(key: key);
+
+  @override
+  State<PageHistoire> createState() => _PageHistoireState();
+}
+
+class _PageHistoireState extends State<PageHistoire> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.png'),
+            fit: BoxFit.cover
+          )
+        ),
+        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children:<Widget> [
+              Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    cerveau.getStory(),
+                    style: TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ),
+              ),
+
+              Expanded(
+                flex: 2,
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      cerveau.nextStory(1);
+                    });
+                  },
+                  style: TextButton.styleFrom(backgroundColor: Colors.red),
+                  child: Text(
+                    cerveau.getChoixUn(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.4,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+
+              Expanded(
+                flex: 2,
+                child: Visibility(
+                  visible: cerveau.buttonShouldBeVisible(),
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        cerveau.nextStory(1);
+                      });
+                    },
+                    style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                    child: Text(
+                      cerveau.getChoixDeux(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.4,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
